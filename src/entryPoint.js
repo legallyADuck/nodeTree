235cd1039df422a59fedb7634635161cca302640
cori <tiziano313rivadeneira@gmail.com>
2026-03-29 16:40:10 -0300

    added level order for each

--- src/entryPoint.js
+++ src/entryPoint.js
@@ -13,63 +13,113 @@ class Node {
 
 class Tree {
   constructor(array = []) {
-    // default is []
     if (array instanceof Array) this.array = array; // strings or numbers are not allowed
     this.root = new Node();
   }
 
-  static build(array, node, i = 0) {
-    node.value = array[i];
-    if (!node.value) return;
+  static buildTreeRecursive(arr, start, end) {
+    if (start > end) return null;
 
-    if (array[i + 1] > node.value) {
-      node.r = new Node(array[i + 1]);
-      i++;
-      this.build(array, node.r, i);
-    } else if (array[i + 1] < node.value) {
-      node.l = new Node(array[i + 1]);
-      i++;
-      this.build(array, node.l, i);
-    }
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(arr[mid]);
+
+    root.l = this.buildTreeRecursive(arr, start, mid - 1);
+    root.r = this.buildTreeRecursive(arr, mid + 1, end);
+
+    return root;
+  }
+
+  levelOrderForEach(callback) {
+    // were passing a function, then we execute that function with certain parameters
+    let nodesQueue = [this.root]; // root is the starting node
+
+    (function recursive(node = nodesQueue[0]) {
+      if (nodesQueue.length === 0) return;
+      callback(node.value);
+
+      if (node.l) nodesQueue.push(node.l);
+      if (node.r) nodesQueue.push(node.r);
+      nodesQueue.shift() // remove parent from queue
+
+      recursive()
+    })();
+  }
+
+  sort(array) {
+    const sortedArray = array
+      .sort()
+      .filter((num, index) => num !== array[index + 1]);
+
+    this.array = sortedArray;
+    return sortedArray;
   }
 
   buildTree(array = this.array) {
-    array = array.sort().filter((num, index) => num !== array[index + 1]);
-    Tree.build(array, this.root);
+    array = this.sort(array);
+    this.root = Tree.buildTreeRecursive(array, 0, array.length - 1);
   }
 
-  // buildTree(array = this.array) {
-  //   // use the constructor array if no array is given
-  //   // im using bfs for the node tree, nodes by layers
-  //   array = array.sort().filter((num, index) => num !== array[index + 1]);
+  includes(value, node = this.root) {
+    if (!node) return false;
+    if (node.value === value) return true;
 
-  //   let queue = { lastLayer: [this.root], buildingNewLayer: [] }; // root is the starting value
+    let bool = this.includes(value, node.l);
+    if (bool === false) bool = this.includes(value, node.r);
 
-  //   while (array.length != 0) {
-  //     for (const node of queue.lastLayer) {
-  //       // we start grabbing the values of the array from the starting position
-  //       if (array[0] === undefined) return;
+    return bool;
+  }
 
-  //       node.left = new Node(array[0]);
-  //       array.shift();
+  insert(value, node = this.root) {
+    if (value > node.value && node.r) this.insert(value, node.r);
+    else if (value > node.value) node.r = new Node(value);
 
-  //       node.right = new Node(array[0]);
-  //       array.shift();
+    if (value < node.value && node.l) this.insert(value, node.l);
+    else if (value < node.value) node.l = new Node(value);
+  }
 
-  //       queue.buildingNewLayer.push(node.left, node.right);
-  //     }
+  remove(value, node = this.root) {
+    if (value > node.value && node.r) {
+      if (value === node.r.value) {
+        delete node.r;
+        return;
+      }
+      this.remove(value, node.r);
+    }
 
-  //     // after finishing building, we store it as our last layer for our next iteration
-  //     queue.lastLayer = Array.from(queue.buildingNewLayer); // otherwise it stores a reference value, i want a copy
-  //     queue.buildingNewLayer.length = 0;
-  //   }
+    if (value < node.value && node.l) {
+      if (value === node.l.value) {
+        delete node.l;
+        return;
+      }
+      this.remove(value, node.l);
+    }
 
-  // }
+    return; // if there isnt a node.l or node.r, return, and dont do anything;
+  }
 }
 
-const array = [9, 8, 7, 6, 6, 5, 4, 3, 2, 1];
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const tree = new Tree(array);
 
 tree.buildTree();
-console.log(tree);
+console.log(tree.includes(9));
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.r, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  prettyPrint(node.l, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
+
+tree.insert(4.5);
+
+tree.remove(4.5);
+prettyPrint(tree.root);
+
+tree.levelOrderForEach((value) => {
+  console.log(value);
+});
